bafa6a16c65ae7bd795cd5e66ed9919e8d804876
joseaugusto <joseaugusto0242@gmail.com>
2024-07-03 10:31:21 -0300

    feat: add calculo de adição

--- calculo.js
+++ calculo.js
@@ -1,4 +1,8 @@
+const prompt = require("prompt-sync")();
+
 let opcao;
+let num1;
+let num2;
 
 console.log("O que desdeja calcular?");
 console.log("1. Adição");
@@ -11,26 +15,39 @@ process.stdin.on("data", function (data) {
     let entrada = data.toString().trim();
     if (!opcao) {
         opcao = entrada
+        switch (opcao) {
+            case "1":
+                console.log("Adição: Pressione enter para começar! ")
+                break;
+            case "2":
+                console.log("Subtração: Digite os numeros a serem calculados: ")
+                break;
+            case "3":
+                console.log("Multiplicação: Digite os numeros a serem calculados: ")
+                break;
+            case "4":
+                console.log("Divisão: Digite os numeros a serem calculados: ")
+                break;
+            case "5":
+                console.log("Porcentagem: Digite os numeros a serem calculados: ")
+                break;
+        }
     }
-    if (!opcao)
-        opcao !== 'number'
-    console.log("Opcção inválida! Digite novamente por favor")
-    switch (opcao) {
-        case "1":
-            console.log("Adição: Digite os numeros a serem calculados: ")
-            break;
-        case "2":
-            console.log("Subtração: Digite os numeros a serem calculados: ")
-            break;
-        case "3":
-            console.log("Multiplicação: Digite os numeros a serem calculados: ")
-            break;
-        case "4":
-            console.log("Divisão: Digite os numeros a serem calculados: ")
-            break;
-        case "5":
-            console.log("Porcentagem: Digite os numeros a serem calculados: ")
-            break;
+    else {
+        switch (opcao) {
+            case "1":
+                let num1 = +prompt("Digite o primerio numero: ");
+                if (isNaN(num1)) {
+                    console.log("Opção invalida, tente novamente...")
+                    break;
+                }
+                let num2 = +prompt("Digite o segundo numero: ");
+                if (isNaN(num2)) {
+                    console.log("Opção invalida, tente novamente...")
+                }
+                let resultado = num1 + num2
+                console.log(resultado)
+                break;
+        }
     }
 })
-
